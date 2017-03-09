a94075361608ec0c21df5e3c92160e182ad154cf
Sagar Mangulkar <smangul1@sl-snow.pods.bu.int>
2017-03-09 17:30:10 -0500

    Initial Commit

---
+++ SideScrollingGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SideScrollingGame
+//
+//  Created by Sagar Mangulkar on 3/9/17.
+//  Copyright © 2017 Sagar Mangulkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SideScrollingGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SideScrollingGame
+//
+//  Created by Sagar Mangulkar on 3/9/17.
+//  Copyright © 2017 Sagar Mangulkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
