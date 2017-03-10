862304e70090a6087c547af3d4eb4d26bf03ba59
Sagar Mangulkar <smangul1@sl-snow.pods.bu.int>
2017-03-09 23:31:17 -0500

    Implement all images and views

---
+++ SideScrollingGame/PlayViewController.h
@@ -0,0 +1,15 @@
+//
+//  PlayViewController.h
+//  SideScrollingGame
+//
+//  Created by Sagar Mangulkar on 3/9/17.
+//  Copyright © 2017 Sagar Mangulkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIImageView *imageHeroCar;
+@property (strong, nonatomic) IBOutlet UISlider *slider;
+
+@end
---
+++ SideScrollingGame/PlayViewController.m
@@ -0,0 +1,48 @@
+//
+//  PlayViewController.m
+//  SideScrollingGame
+//
+//  Created by Sagar Mangulkar on 3/9/17.
+//  Copyright © 2017 Sagar Mangulkar. All rights reserved.
+//
+
+#import "PlayViewController.h"
+
+@interface PlayViewController ()
+
+@end
+
+@implementation PlayViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    [NSTimer scheduledTimerWithTimeInterval:0.5
+                                     target:self
+                                   selector:@selector(MovingCarBySlider:)
+                                   userInfo:nil
+                                    repeats:YES];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void)MovingCarBySlider:timer{
+    NSLog(@"Hello");
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
