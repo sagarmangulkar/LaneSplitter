9281ad9373c0d0b51db1fda19b3802a54f8db1c8
Sagar Mangulkar <smangul1@bl-n05.pods.bu.int>
2017-03-10 19:17:30 -0500

    Implement intersection of two cars

--- SideScrollingGame/PlayViewController.m
+++ SideScrollingGame/PlayViewController.m
@@ -18,11 +18,11 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    [NSTimer scheduledTimerWithTimeInterval:0.5
-                                     target:self
-                                   selector:@selector(MovingCarBySlider:)
-                                   userInfo:nil
-                                    repeats:YES];
+//    [NSTimer scheduledTimerWithTimeInterval:3.0
+//                                     target:self
+//                                   selector:@selector(MovingCarBySlider:)
+//                                   userInfo:nil
+//                                    repeats:YES];
     [NSTimer scheduledTimerWithTimeInterval:3.0
                                      target:self
                                    selector:@selector(OtherCar1:)
@@ -37,15 +37,51 @@
 
 -(void)MovingCarBySlider:timer{
     [_labelTest setHidden:YES];
+//    _labelTest.text = @"Changed";
+//    NSLog(@"Hello, @%f", _slider.value * 327);
+//    [UIView animateWithDuration:0.5 animations:^{
+//    CGRect imageFrameHeroCar = _imageHeroCar.frame;
+//        imageFrameHeroCar.origin.x = _slider.value * 327;
+//        _imageHeroCar.frame = imageFrameHeroCar;
+//        }];
+////    [UIView commitAnimations];
+    
+    //just a testing
+//    [UIView animateWithDuration:3.0 animations:^{
+//        CGRect frameHeroCar = _imageHeroCar.frame;
+//        frameHeroCar.origin.x = frameHeroCar.origin.x+ 1;
+//        _imageHeroCar.frame = frameHeroCar;
+//    }];
+//    [UIView commitAnimations];
+
+}
+- (IBAction)SliderValueChanged:(id)sender {
     _labelTest.text = @"Changed";
     NSLog(@"Hello, @%f", _slider.value * 327);
+    
+    //moving hero car right-left
     [UIView animateWithDuration:0.5 animations:^{
-    CGRect imageFrameHeroCar = _imageHeroCar.frame;
+        CGRect imageFrameHeroCar = _imageHeroCar.frame;
         imageFrameHeroCar.origin.x = _slider.value * 327;
         _imageHeroCar.frame = imageFrameHeroCar;
-        }];
+    }];
     [UIView commitAnimations];
     
+    //checking for crash
+//    CGRect heroCarFrame = _imageHeroCar.frame;
+//    CGRect otherCar1 = _imageOtherCar1.frame;
+    CALayer *presentationLayerHeroCar = (CALayer*)[_imageHeroCar.layer presentationLayer];
+    CALayer *presentationLayerOtherCar1 = (CALayer*)[_imageOtherCar1.layer presentationLayer];
+
+    if (CGRectIntersectsRect(presentationLayerHeroCar.frame, presentationLayerOtherCar1.frame)) {
+        [UIView animateWithDuration:0.5 animations:^{
+            CGRect frameHeroCar = _imageHeroCar.frame;
+            frameHeroCar.origin.y = 100;
+            _imageHeroCar.frame = frameHeroCar;
+        }];
+        [UIView commitAnimations];
+    }
+
 }
 
 -(void)OtherCar1:timer{
